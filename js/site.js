d6af108453703ddebd0185e594ef5c6468dc2701
Brian Sarbin <bsarbin@cisco.com>
2016-05-22 21:41:59 -0700

    Major changes under the hood. Added content for the major sections. Need to check content and to add images.

--- js/site.js
+++ js/site.js
@@ -1,9 +1,21 @@
-/*$('a[href^="#"]').on('click', function(event) {
-    var target = $(this.href);
-    if( target.length ) {
-        event.preventDefault();
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+
+      //Only animate the top navigation
+      if (target.length && 
+         (target.selector == "#our-story"     || 
+          target.selector == "#event-details" ||
+          target.selector == "#getting-here"  ||
+          target.selector == "#things-to-do"  ||
+          target.selector == "#registry")) {
         $('html, body').animate({
-            scrollTop: target.offset().top
+          scrollTop: target.offset().top
         }, 1000);
+        return false;
+      }
     }
-});*/
+  });
+});
